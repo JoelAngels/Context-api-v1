f2a674ac0cf61ee13a5eea0bf704f38351de7361
Joel Angel <joelbaraka4325@gmail.com>
2023-07-21 15:23:48 +0300

    changed the layout code to the right one

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { IoShirtOutline } from "react-icons/io5";
 import { AiFillShopping } from "react-icons/ai";
 
-import { Link } from "react-router-dom";
-
 function Card({ name, price }) {
+  const item = { name: "tshirt", price: 20 };
   return (
     <div className="card">
       <div className="product-box">
