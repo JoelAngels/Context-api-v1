97dec283195949efe7c6dd4a335aa2d9f6dca3ca
Joel Angel <joelbaraka4325@gmail.com>
2023-07-21 16:43:06 +0300

    passed the context and connected it successfully

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,11 +2,18 @@ import CardContext from "../CardContext";
 import { useContext } from "react";
 
 function Checkout() {
-    const
+  const { items } = useContext(CardContext);
   return (
     <div>
       <h2>Checkout</h2>
-      <div className="shopping-bag"></div>
+      <div className="product-list">
+        {items.map((item) => (
+          <div>
+            <h2>{item.name}</h2>
+            <h2>{item.price}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
